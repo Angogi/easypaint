1951fff7083a983b1b319d8738a04c9795b5d11a
angogi <andresmahanaim@gmail.com>
2021-08-14 15:56:27 +0200

    add script google2

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,18 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          {/* Global site tag (gtag.js) - Google Analytics */}
+          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-202506187-1">
+          </script>
+          <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'UA-202506187-1');
+          </script>
+        </Head>
         <body>
           <Main />
           <NextScript />
